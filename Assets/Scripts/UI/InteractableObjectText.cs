f89bb90fd89a497433e26ac358b492b7b883a3ff
Route-Paradise <lappyparadise1341@gmail.com>
2024-10-14 00:14:09 +0900

    [Add]InteractableTextUI

---
+++ Assets/Scripts/UI/InteractableObjectText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+namespace UIControls
+{
+    public class InteractableObjectText : MonoBehaviour,IInitializable
+    {
+        [SerializeField] private Text _text;
+        [SerializeField] private DetectInteract _detector;
+
+        public void Initialize()
+        {
+            _detector.OnInInteractableRange
+                .Subscribe(data =>
+                {
+                    if (data == null) { gameObject.SetActive(false); }
+                    else
+                    {
+                        gameObject.SetActive(true);
+                        _text.text = data._inRangeText;
+                    }
+                }).AddTo(this);
+        }
+    }
+}
