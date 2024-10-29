ccb8ea057ee4c6a4c19f2f9a5838e0ff302f2adb
HiguchiHaruya <tongkouyangye@gmail.com>
2024-10-30 03:28:20 +0900

    スイッチの上に乗ったらドアが開く処理&ドアモデル

--- Assets/Scripts/StageGimmick/CandleTest/SwitchTest.cs
+++ Assets/Scripts/StageGimmick/CandleTest/SwitchTest.cs
@@ -1,33 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchTest : MonoBehaviour
 {
-    private bool hasProcessed = false;
-    private float rayDistance = 1f;
+    private bool _hasProcessed = false;
+    private float _rayDistance = 1f;
+    public event Action OnSwitchPressed; //ドア開閉通知
     private void Update()
     {
-        if (hasProcessed || !gameObject) return;
+        if (_hasProcessed || !gameObject) return;
 
         if (IsPlayerOnSwitch())
         {
-            OpenDoor();
-            hasProcessed = true;
+            OnSwitchPressed?.Invoke();
+            _hasProcessed = true;
         }
     }
     private bool IsPlayerOnSwitch()
     {
         Ray ray = new Ray(transform.position + new Vector3(0, transform.localScale.y / 2, 0), Vector3.up);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, rayDistance))
+        if (Physics.Raycast(ray, out hit, _rayDistance))
         {
             return true;
         }
         return false;
-    }
-    private void OpenDoor()
-    {
-        Debug.Log("ドアオープン!");
     }
 }
--- Assets/Scripts/StageGimmick/StageGimmickObserver.cs
+++ Assets/Scripts/StageGimmick/StageGimmickObserver.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEditorInternal;
 using UnityEngine;
-
 /// <summary>
 /// すべてのギミックをクリアしたときに通知をするクラス
 /// </summary>
@@ -14,7 +13,8 @@ public class StageGimmickObserver : MonoBehaviour
     [Header("現在の階層のギミック")]
     [SerializeField] private StageGimmickBase[] _stageGimmicks;
 
-    [SerializeField] private GameObject _clearSwitch;
+    [SerializeField] private SwitchTest _switchTest;
+    [SerializeField] private DoorTest _doorTest;
 
     // キャンセル用のトークンソース
     private CancellationTokenSource _cancellationTokenSource;
@@ -24,7 +24,12 @@ public class StageGimmickObserver : MonoBehaviour
 
     private void Start()
     {
-        _clearSwitch?.SetActive(false);
+        if (_switchTest != null && _doorTest != null)
+        {
+            _switchTest.OnSwitchPressed += _doorTest.OpenDoor;
+            _switchTest?.gameObject.SetActive(true);
+        }
+
         if (_stageGimmicks != null)
         {
             //キャンセレーショントークンソースの初期化
@@ -71,7 +76,7 @@ public class StageGimmickObserver : MonoBehaviour
     {
         Debug.Log("すべてのギミックがクリアされました！");
         OnAllGimmicksClear?.Invoke(); // イベントを発行
-        _clearSwitch.SetActive(true);
+        _switchTest?.gameObject.SetActive(true);
     }
 
     private void OnDisable()
