32560a3bfc80be2cee41b82947c41e39b341d3e7
HiguchiHaruya <tongkouyangye@gmail.com>
2024-10-30 17:23:27 +0900

    ドアのアニメーションとプレハブ変更 判定方法をBoxCastに変更 Gizmosで可視化

--- Assets/Scripts/StageGimmick/CandleTest/SwitchTest.cs
+++ Assets/Scripts/StageGimmick/CandleTest/SwitchTest.cs
@@ -7,6 +7,10 @@ public class SwitchTest : MonoBehaviour
 {
     private bool _hasProcessed = false;
     private float _rayDistance = 1f;
+    private float _switchRadius = 0.5f; //スイッチの半径
+    private float _switchHeight = 0.5f; //高さの半径
+    private float _switchDepth = 1f; //奥行き
+    private Vector3 _switchCenterOffset = new Vector3(0, 0.5f, 0);
     public event Action OnSwitchPressed; //ドア開閉通知
     private void Update()
     {
@@ -20,12 +24,12 @@ public class SwitchTest : MonoBehaviour
     }
     private bool IsPlayerOnSwitch()
     {
-        Ray ray = new Ray(transform.position + new Vector3(0, transform.localScale.y / 2, 0), Vector3.up);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, _rayDistance))
-        {
-            return true;
-        }
-        return false;
+        Vector3 halfExtents = new Vector3(_switchRadius, _switchHeight, _switchDepth);
+        return Physics.BoxCast(transform.position + new Vector3(0, -0.5f, 0), halfExtents, Vector3.up, out RaycastHit hit, Quaternion.identity);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + new Vector3(0, 1f, 0), Vector3.one);
     }
 }
