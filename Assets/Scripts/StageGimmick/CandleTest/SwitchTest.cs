56c16e74ffeacefb909d2c7e3ae79e6589282c1d
HiguchiHaruya <tongkouyangye@gmail.com>
2024-11-06 14:02:30 +0900

    プレイヤーをレイヤーで判定

--- Assets/Scripts/StageGimmick/CandleTest/SwitchTest.cs
+++ Assets/Scripts/StageGimmick/CandleTest/SwitchTest.cs
@@ -9,6 +9,7 @@ public class SwitchTest : MonoBehaviour
     private float _rayDistance = 1f;
     private float _switchDistance = 2f;
     [SerializeField] private Vector3 _halfExtents; //スイッチの大きさの半径
+    [SerializeField] private LayerMask playerLayer;
     private Vector3 _switchCenterOffset = new Vector3(0, 0.5f, 0);
     public event Action OnSwitchPressed; //ドア開閉通知
     private void FixedUpdate()
@@ -23,7 +24,14 @@ public class SwitchTest : MonoBehaviour
     }
     private bool IsPlayerOnSwitch()
     {
-        return Physics.BoxCast(transform.position + new Vector3(0, -0.5f, 0), _halfExtents, Vector3.up, out RaycastHit hit, Quaternion.identity, _switchDistance);
+        if (Physics.BoxCast(transform.position + new Vector3(0, -0.5f, 0), _halfExtents, Vector3.up, out RaycastHit hit, Quaternion.identity, _switchDistance))
+        {
+            if ((playerLayer.value & (1 << hit.collider.gameObject.layer)) != 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
     private void OnDrawGizmos()
     {
