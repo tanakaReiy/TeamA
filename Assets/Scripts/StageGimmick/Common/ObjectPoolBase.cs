1a2a870bde51b33c182b421f95859dc292fc6607
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-11-27 15:36:21 +0900

    [add]ドーナッツとオブジェクトプールの処理を追加

---
+++ Assets/Scripts/StageGimmick/Common/ObjectPoolBase.cs
@@ -0,0 +1,55 @@
+﻿using Alchemy.Inspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectPoolBase<T> : MonoBehaviour where T : Component
+{
+    [LabelText("プールするオブジェクトプレハブ")]
+    [SerializeField] protected T _objectPrefab;
+    [LabelText("初期プール数")]
+    [SerializeField] protected int _initialSize = 10;
+
+    private Queue<T> _pool;
+
+    protected virtual void Awake()
+    {
+        _pool = new Queue<T>();
+
+        //初期プールを作成
+        for (int i = 0; i < _initialSize; i++)
+        {
+            T obj = CreateNewObject();
+            _pool.Enqueue(obj);
+        }
+    }
+
+    // 新しいオブジェクトを作成
+    private T CreateNewObject()
+    {
+        T obj = Instantiate(_objectPrefab);
+        obj.gameObject.SetActive(false);
+        return obj;
+    }
+
+    // オブジェクトを取得
+    public T Get()
+    {
+        if (_pool.Count > 0)
+        {
+            T obj = _pool.Dequeue();
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+
+        // プールが空の場合、新規作成
+        return CreateNewObject();
+    }
+
+    // オブジェクトをプールに戻す
+    public void ReturnToPool(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        _pool.Enqueue(obj);
+    }
+}
---
+++ Assets/Scripts/StageGimmick/Donuts/DonutsObjectPool.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonutsObjectPool : ObjectPoolBase<Donuts>
+{
+    //何かカスタマイズしたい場合に書く
+}
